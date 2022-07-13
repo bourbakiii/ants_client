2261fb84420bad0ce4c22a8c8ff72741bfac9ef6
bourbakiii <55923749+bourbakiii@users.noreply.github.com>
2022-07-13 22:24:24 +0300

    Initial commit with installed packages

---
+++ nuxt.config.js
@@ -0,0 +1,66 @@
+export default {
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'ants_client',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+    '@assets/scss/style.scss'
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+  ],
+  styleResources: {
+    scss: [
+      '@assets/scss/variables.scss'
+    ]
+  },
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: ['@nuxtjs/style-resources', 'cookie-universal-nuxt', '@nuxtjs/axios', '@nuxt/image'],
+
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {
+    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
+    baseURL: '/',
+  },
+
+  // PWA module configuration: https://go.nuxtjs.dev/pwa
+  pwa: {
+    manifest: {
+      lang: 'en'
+    }
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+  },
+  pageTransition: {
+    name: 'page',
+    mode: 'out-in',
+    appear: true
+  },
+  loading: {
+    failedColor: '#F95738',
+    color: '#0EA976',
+    height: '5px',
+    throttle: 0
+  },
+}
